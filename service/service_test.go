5c3f4d6b5721c045e74cc2a10716cb52928a1e1a
blacksheepaul <26378591+blacksheepaul@users.noreply.github.com>
2026-06-17 00:40:27 +0800

    test: use newTestService in service_test

--- service/service_test.go
+++ service/service_test.go
@@ -2,15 +2,10 @@ package service
 
 import (
 	"testing"
-
-	"github.com/blacksheepaul/timelog/internal/adapter"
-	"github.com/blacksheepaul/timelog/internal/testutil"
 )
 
-func TestNewServiceWiresInjectedRepositories(t *testing.T) {
-	dao := testutil.NewTestDAO(t)
-	repos := adapter.NewRepositories(dao)
-	svc := NewService(repos, repos, repos, repos, repos, repos, repos, repos, repos, testutil.FakeLogger{}, testutil.NewTestConfig(), nil)
+func TestNewServiceCreatesNonNilService(t *testing.T) {
+	svc, _ := newTestService(t)
 	if svc == nil {
 		t.Fatal("expected non-nil service")
 	}
